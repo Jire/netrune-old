cbd7863d3daa9cedaf18b37fa292d113baeca0a4
Jire <thomasgnappo@gmail.com>
2023-02-20 13:30:54 -0500

    Try using `maven-publish` plugin on JitPack

--- buffer/build.gradle.kts
+++ buffer/build.gradle.kts
@@ -1,7 +1,18 @@
 plugins {
     netrune
+    `maven-publish`
 }
 
 dependencies {
     api(libs.openrs2.buffer)
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+
+        pom {
+            packaging = "jar"
+        }
+    }
 }
