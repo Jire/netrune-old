b6238e142c4982d4f43356ffb9c1257fee3a0d62
Jire <thomasgnappo@gmail.com>
2023-02-20 14:03:34 -0500

    Configured `JavaCompile` with `--enable-preview` for Panama build, and moved Maven publish configurations to `netrune` Gradle build plugin

--- build-src/project-plugin/src/main/kotlin/project.kt
+++ build-src/project-plugin/src/main/kotlin/project.kt
@@ -1,11 +1,30 @@
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.withType
 
 const val NETRUNE_DEFAULT_GROUP = "org.jire.netrune"
 const val NETRUNE_DEFAULT_VERSION = "0.1.0-SNAPSHOT"
 
 internal fun Project.netrune() {
     plugins.apply("org.jetbrains.kotlin.jvm")
+    plugins.apply("org.gradle.maven-publish")
 
     group = NETRUNE_DEFAULT_GROUP
     version = NETRUNE_DEFAULT_VERSION
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.compilerArgs = mutableListOf("--enable-preview")
+    }
+
+    val javaComponent = components.findByName("java") ?: return
+    configure<PublishingExtension> {
+        publications.create<MavenPublication>("maven") {
+            from(javaComponent)
+        }
+    }
 }
